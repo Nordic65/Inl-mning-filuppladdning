d0aa45feb5fdf023ef947a4cb70aad545b7f3aa0
Samuelknis <90838614+Nordic65@users.noreply.github.com>
2022-10-14 15:44:22 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>username = pirate123; password = 123</p>
+    <form method="post" action="data.php" enctype="multipart/form-data">
+        <lable for="email">Enter thy username</lable>
+        <br>
+        <input type="text" name="user">
+        <br>
+        <lable for="password">Enter thy password for thy name</lable>
+        <br>
+        <input type="text" name="password">
+        <br>
+        <input type="submit" value="Press me you scallywag" name="submit">
+    </form>
+</body>
+
+</html>
